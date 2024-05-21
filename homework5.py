3ca8912e40adc39eee1ffd3f15a64f34577bfbc9
gorm3531 <mcgermafrodit@gmail.com>
2024-05-21 23:58:34 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['apple', 'orange', 'banana', 'pineapple', 'grapefruit', 'kiwi', 'mango']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[3:6])
+my_list[3] = 6
+print(my_list) #2
+my_dict = {'Andrew': 88005553535, 'Alina': 88005553534, 'Karina': 8800553533}
+print(my_dict)
+my_dict.update({'Alice': 88005553333, 'Lisa': 88005533333})
+print(my_dict)
+my_dict ['Alice'] = 3531
+print(my_dict) #3
